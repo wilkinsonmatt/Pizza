b4bf62a08f1af438d70837fdfc7245a1ee4fb5d7
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-17 09:25:17 -0700

    added outline that determens the cost of the pizza

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,31 @@ function Pizza() {
   this.size = "medium";
   this.toppings = [];
   this.numberOfToppings = 0;
-  this.cost = 5.00;
+  this.cost = 8.00;
 }
 
-pizzz_options {
+pizza_options {
   size = ["small", "medium", "large"];
   toppings = ["cheese", "pepperoni", "artichoke", "anchovy", "etc"];
 }
 
+function costOfPizza();
+  let costOfPizza = 0;
 
+  if(/*pizza is a small*/) {
+    costOfPizza = 8.00;
+    if(/*number of toppings is more than 3*/) {
+      costOfPizza += (.5 * /*number of toppings - 3*/);
+  }else if(/*pizza is a med*/) {
+    costOfPizza = 8.00;
+    if(/*number of toppings is more than 3*/) {
+      costOfPizza += (.5 * /*number of toppings - 3*/);
+    }
+  }else if(/*pizza is a large*/) {
+    costOfPizza = 10.00;
+    if(/*number of toppings is more than 3*/) {
+      costOfPizza += (.5 * /*number of toppings - 3*/);
+    }
+  }
+  return costOfPizza;
+}
