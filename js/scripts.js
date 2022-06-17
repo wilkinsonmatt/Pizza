880e93b91847e2bc035affd85413959b8c2d4eef
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-17 12:29:50 -0700

    fixed bugs affer running the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let currentOrder;
 
-// Object contains the pizza(s) ordered (sorted as objects in an array), and the total cost of the order
+//  Object contains the pizza(s) ordered (sorted as objects in an array), and the total cost of the order
 function Order() {
   this.pizzas = [];
   this.totalCostOfOrder = 0;
@@ -10,9 +10,9 @@ function Order() {
 //  - makeAPizza makes the pizza
 //  - costOfPizza sets the cost for that pizza
 Order.prototype.AddPizza = function(pizzaSize, pizzaToppings){
-  let newpizza = new Pizza;
-  newpizza.makeAPizza(pizzaSize, pizzaToppings);
-  newpizza.costOfPizza();
+  let newPizza = new Pizza;
+  newPizza.makeAPizza(pizzaSize, pizzaToppings);
+  newPizza.costOfPizza();
   this.pizzas.push(newPizza);
 }
 
@@ -23,7 +23,7 @@ Order.prototype.CostOfOrder = function(){
   }
 }
 
-// Object contains size of pizza, list of top toppings, cost of that pizza based on size/toppings
+//  Object contains size of pizza, list of top toppings, cost of that pizza based on size/toppings
 function Pizza() {
   this.size = "";
   this.toppings = [];
@@ -31,7 +31,7 @@ function Pizza() {
 }
 
 //  Prototype sets that pizza's size and list of toppings 
-Pizza.prototype.makeAPizza(pizzaSize, pizzaToppings) = function() {
+Pizza.prototype.makeAPizza = function(pizzaSize, pizzaToppings) {
   this.size = pizzaSize;
   this.toppings = pizzaToppings;
 }
@@ -39,7 +39,7 @@ Pizza.prototype.makeAPizza(pizzaSize, pizzaToppings) = function() {
 //  Prototype comes up with the cost of that pizza
 //  - Each pizza size has a base cost
 //  - adds $0.50 for each topping if they have more than 3
-Pizza.prototype.costOfPizza() = function(){
+Pizza.prototype.costOfPizza = function(){
   if(this.size === "small") {
     this.cost = 5.00;
     if(this.toppings.length > 3) {
@@ -48,7 +48,7 @@ Pizza.prototype.costOfPizza() = function(){
   }else if(this.size === "medium") {
     this.cost = 8.00;
     if(this.toppings.length > 3) {
-      this.cost += (.5 * (this.toppings.length - 3)/);
+      this.cost += (.5 * (this.toppings.length - 3));
     }
   }else if(this.size === "large") {
     this.cost = 10.00;
@@ -64,12 +64,13 @@ Pizza.prototype.costOfPizza() = function(){
 /************************ UI LOGIC **************************************/
 //all the options the customer had choose from to customize their pizza
 const pizza_options = {
-  size: ["small", "medium", "large"];
-  toppings: ["cheese", "pepperoni", "artichoke", "anchovy", "etc"];
-}
+  size: ["small", "medium", "large"],
+  toppings: ["cheese", "pepperoni", "artichoke", "anchovy", "etc"],
+};
 
 
 //start new order button ---> goes to window with size and pizza toppings
+
 currentOrder = new Order();
 currentOrder.AddPizza();
 //submit button to add pizza to order
