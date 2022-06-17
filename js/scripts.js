b1b540ddbb956b7b1032e35924f30ae6ee77b16c
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-17 09:06:03 -0700

    added objects for pizza and for the total order

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function Order() {
+  this.pizzas = [];
+  this.costOfOrder = 0;
+}
+
+function Pizza() {
+  this.size = "medium";
+  this.toppings = [];
+  this.numberOfToppings = 0;
+  this.cost = 5.00;
+}
+
+pizzz_options {
+  size = ["small", "medium", "large"];
+  toppings = ["cheese", "pepperoni", "artichoke", "anchovy", "etc"];
+}
+
+
